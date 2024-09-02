8087eecf638c0ca78f4d5863fe1d1bfeca5cf041
Guyutongxue <guyutongxue@163.com>
2024-09-02 14:01:26 +0800

    brand branch created

--- library/config.sh
+++ library/config.sh
@@ -36,11 +36,11 @@
 # **************************************************************************
 
 MINGW_W64_BUILDS_VERSION="MinGW-W64-builds-5.0.0"
-MINGW_W64_PKG_STRING="Built by MinGW-Builds project"
+MINGW_W64_PKG_STRING="Built by Guyutongxue"
 
 # **************************************************************************
 
-PROJECT_ROOT_URL=https://github.com/niXman/mingw-builds
+PROJECT_ROOT_URL=https://github.com/guyutongxue/mingw-builds
 BUG_URL=$PROJECT_ROOT_URL
 PROJECT_FS_ROOT_DIR=/home/frs/project/mingw-w64
 
